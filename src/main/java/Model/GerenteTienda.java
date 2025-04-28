4695df2e803a30aff55f02c91d4cc0837b28e533
Diego Cruces <diegocruces1002@gmail.com>
2025-04-28 10:33:14 -0400

    modelo gerente de tienda

---
+++ src/main/java/Model/GerenteTienda.java
@@ -0,0 +1,30 @@
+package Model;
+
+public class GerenteTienda extends Usuario {
+    private int idGerente;
+    private String tiendaAsignada;
+
+    public GerenteTienda() {}
+
+    public GerenteTienda(String nombre, String email, int idGerente, String tiendaAsignada) {
+        super(nombre, email);
+        this.idGerente = idGerente;
+        this.tiendaAsignada = tiendaAsignada;
+    }
+
+    public int getIdGerente() {
+        return idGerente;
+    }
+
+    public void setIdGerente(int idGerente) {
+        this.idGerente = idGerente;
+    }
+
+    public String getTiendaAsignada() {
+        return tiendaAsignada;
+    }
+
+    public void setTiendaAsignada(String tiendaAsignada) {
+        this.tiendaAsignada = tiendaAsignada;
+    }
+}
