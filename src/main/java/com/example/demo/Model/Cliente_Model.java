835a76b92fad6c2fdc6b306e65a000bf94c34479
Dego <diegocruces1002@gmail.com>
2025-05-05 10:52:55 -0400

    Update Cliente_Model.java
    modelo cliente listo

--- src/main/java/com/example/demo/Model/Cliente_Model.java
+++ src/main/java/com/example/demo/Model/Cliente_Model.java
@@ -2,10 +2,11 @@ package com.example.demo.Model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
-
+@NoArgsConstructor
 public class Cliente_Model extends Usuario_Model {
     private String idCliente;
     private String direccionEnvio;
