13ae73c88ca668ebffe75e068da756cc32992e13
Diego Cruces <diegocruces1002@gmail.com>
2025-05-19 18:28:45 -0400

    cambiado Cliente_Model

--- src/main/java/com/example/demo/Model/Cliente_Model.java
+++ src/main/java/com/example/demo/Model/Cliente_Model.java
@@ -1,5 +1,9 @@
 package com.example.demo.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,7 +11,10 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+
+@Entity
 public class Cliente_Model extends Usuario_Model {
+
     private String direccionEnvio;
 
 }
