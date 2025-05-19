252c1a969c6aafaa66a05806c83d1d5e41f89a5e
lgz004 <lgodoyzanelli@gmail.com>
2025-05-19 18:33:23 -0400

    Error de sintaxix corregido

--- src/main/java/com/example/demo/Model/Usuario_Model.java
+++ src/main/java/com/example/demo/Model/Usuario_Model.java
@@ -15,7 +15,7 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Usuario_Model {
     @Id
-    @GeneratedValue(Strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
 private int id;
 private String nombre;
 private String email;
