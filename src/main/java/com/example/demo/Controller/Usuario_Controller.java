25b518284d7bc5ca5701282de19101952310d528
Diego Cruces <diegocruces1002@gmail.com>
2025-05-19 17:17:22 -0400

    cambiado el requestmapping

--- src/main/java/com/example/demo/Controller/Usuario_Controller.java
+++ src/main/java/com/example/demo/Controller/Usuario_Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuarios")
+@RequestMapping("api/v1/usuarios")
 public class Usuario_Controller {
     @Autowired
     private Usuario_Service usuarioService;
