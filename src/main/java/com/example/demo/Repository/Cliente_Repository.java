70382adc0bfb589e30c40b1b9827d0ca9375b5ec
Diego Cruces <diegocruces1002@gmail.com>
2025-05-19 18:30:05 -0400

    cambiado Cliente_Repository

--- src/main/java/com/example/demo/Repository/Cliente_Repository.java
+++ src/main/java/com/example/demo/Repository/Cliente_Repository.java
@@ -1,9 +1,10 @@
 package com.example.demo.Repository;
 
+import com.example.demo.Model.Cliente_Model;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface Cliente_Repository extends JpaRepository<Cliente_Model,Integer> {
+public interface Cliente_Repository extends JpaRepository<Cliente_Model, Integer> {
 
 }
