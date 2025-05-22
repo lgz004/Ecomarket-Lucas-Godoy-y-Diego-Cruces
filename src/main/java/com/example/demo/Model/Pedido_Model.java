dfc836db2be5f51c905e97bec6005096b2643427
lgz004 <lgodoyzanelli@gmail.com>
2025-05-22 14:59:05 -0400

    Pedido_Model hecho

---
+++ src/main/java/com/example/demo/Model/Pedido_Model.java
@@ -0,0 +1,22 @@
+package com.example.demo.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Entity
+public class Pedido_Model {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int idPedido;
+    private Double totalPedido;
+}
